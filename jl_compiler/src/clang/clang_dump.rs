21ea6ab898f41f0dd4f4d5b27804d1654daefcb6
vain0x <vainzerox@gmail.com>
2020-06-12 21:48:32 +0900

    refactor: clang_dump::Dx を追加

--- jl_compiler/src/clang/clang_dump.rs
+++ jl_compiler/src/clang/clang_dump.rs
@@ -6,115 +6,162 @@ use std::{
     rc::Rc,
 };
 
-fn write_indent(indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
-    for _ in 0..indent {
-        write!(out, "    ")?;
+/// Dump context.
+pub(crate) struct Dx<W> {
+    w: W,
+    indent: usize,
+}
+
+impl<W: Write> Dx<W> {
+    pub(crate) fn new(w: W) -> Self {
+        Self { w, indent: 0 }
+    }
+
+    pub(crate) fn finish(self) -> W {
+        self.w
+    }
+
+    pub(crate) fn do_with_indent(
+        &mut self,
+        write_fn: impl FnOnce(&mut Self) -> io::Result<()>,
+    ) -> io::Result<()> {
+        self.indent += 1;
+        write_fn(self)?;
+        self.indent -= 1;
+        Ok(())
+    }
+
+    pub(crate) fn do_with_unindent(
+        &mut self,
+        write_fn: impl FnOnce(&mut Self) -> io::Result<()>,
+    ) -> io::Result<()> {
+        let n = self.indent.min(1);
+        self.indent -= n;
+        write_fn(self)?;
+        self.indent += n;
+        Ok(())
+    }
+}
+
+impl<W: Write> Write for Dx<W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.w.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.w.flush()
+    }
+}
+
+fn write_indent(dx: &mut Dx<impl Write>) -> io::Result<()> {
+    for _ in 0..dx.indent {
+        write!(dx, "    ")?;
     }
     Ok(())
 }
 
-fn write_ty(ty: &CTy, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_ty(ty: &CTy, dx: &mut Dx<impl Write>) -> io::Result<()> {
     match ty {
-        CTy::Other(text) => write!(out, "{}", text),
-        CTy::Void => write!(out, "void"),
-        CTy::Int => write!(out, "int"),
+        CTy::Other(text) => write!(dx, "{}", text),
+        CTy::Void => write!(dx, "void"),
+        CTy::Int => write!(dx, "int"),
         CTy::Ptr { ty } => {
-            write_ty(&ty, indent, out)?;
-            write!(out, "*")
+            write_ty(&ty, dx)?;
+            write!(dx, "*")
         }
-        CTy::Struct(name) => write!(out, "struct {}", name),
+        CTy::Struct(name) => write!(dx, "struct {}", name),
     }
 }
 
 /// 変数と型を書き込む。(`int foo` や `void (*f)()` など。)
-fn write_var_with_ty(name: &str, ty: &CTy, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
-    write_ty(ty, indent, out)?;
-    write!(out, " {}", name)
+fn write_var_with_ty(name: &str, ty: &CTy, dx: &mut Dx<impl Write>) -> io::Result<()> {
+    write_ty(ty, dx)?;
+    write!(dx, " {}", name)
 }
 
-fn write_param_list(params: &[(String, CTy)], indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
-    write!(out, "(")?;
+fn write_param_list(params: &[(String, CTy)], dx: &mut Dx<impl Write>) -> io::Result<()> {
+    write!(dx, "(")?;
 
     for (i, (name, ty)) in params.iter().enumerate() {
         if i != 0 {
-            write!(out, ", ")?;
+            write!(dx, ", ")?;
         }
 
-        write_var_with_ty(name, ty, indent, out)?;
+        write_var_with_ty(name, ty, dx)?;
     }
 
-    write!(out, ")")
+    write!(dx, ")")
 }
 
-fn write_expr(expr: &CExpr, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_expr(expr: &CExpr, dx: &mut Dx<impl Write>) -> io::Result<()> {
     match expr {
-        CExpr::IntLit(value) => write!(out, "{}", value),
-        CExpr::Name(name) => write!(out, "{}", name),
+        CExpr::IntLit(value) => write!(dx, "{}", value),
+        CExpr::Name(name) => write!(dx, "{}", name),
         CExpr::Dot { left, field } => {
-            write_expr(left, indent, out)?;
-            write!(out, ".{}", field)
+            write_expr(left, dx)?;
+            write!(dx, ".{}", field)
         }
         CExpr::Arrow { left, field } => {
-            write_expr(left, indent, out)?;
-            write!(out, "->{}", field)
+            write_expr(left, dx)?;
+            write!(dx, "->{}", field)
         }
         CExpr::Call { left, args } => {
-            write_expr(left, indent, out)?;
-            write!(out, "(")?;
+            write_expr(left, dx)?;
+            write!(dx, "(")?;
 
             for (i, arg) in args.iter().enumerate() {
                 if i != 0 {
-                    write!(out, ", ")?;
+                    write!(dx, ", ")?;
                 }
 
-                write_expr(arg, indent, out)?;
+                write_expr(arg, dx)?;
             }
 
-            write!(out, ")")
+            write!(dx, ")")
         }
         CExpr::UnaryOp { op, arg } => {
-            write!(out, "{}", op.as_str())?;
-            write_expr(arg, indent, out)
+            write!(dx, "{}", op.as_str())?;
+            write_expr(arg, dx)
         }
         CExpr::BinaryOp { op, left, right } => {
-            write_expr(left, indent, out)?;
-            write!(out, " {} ", op.as_str())?;
-            write_expr(right, indent, out)
+            write_expr(left, dx)?;
+            write!(dx, " {} ", op.as_str())?;
+            write_expr(right, dx)
         }
     }
 }
 
-fn write_stmt(stmt: &CStmt, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_stmt(stmt: &CStmt, dx: &mut Dx<impl Write>) -> io::Result<()> {
     match stmt {
         CStmt::Expr(body) => {
-            write_expr(body, indent, out)?;
-            write!(out, ";")
+            write_expr(body, dx)?;
+            write!(dx, ";")
         }
-        CStmt::Block(body) => write_block(body, indent, out),
-        CStmt::Goto { label } => write!(out, "goto {};", label),
-        CStmt::Return(None) => write!(out, "return;"),
+        CStmt::Block(body) => write_block(body, dx),
+        CStmt::Goto { label } => write!(dx, "goto {};", label),
+        CStmt::Return(None) => write!(dx, "return;"),
         CStmt::Return(Some(arg)) => {
-            write!(out, "return ")?;
-            write_expr(arg, indent, out)?;
-            write!(out, ";")
+            write!(dx, "return ")?;
+            write_expr(arg, dx)?;
+            write!(dx, ";")
         }
         CStmt::If { cond, body, alt } => {
-            write!(out, "if (")?;
-            write_expr(cond, indent, out)?;
-            write!(out, ") ")?;
-            write_stmt(body, indent, out)?;
-            write!(out, " else ")?;
-            write_stmt(alt, indent, out)
+            write!(dx, "if (")?;
+            write_expr(cond, dx)?;
+            write!(dx, ") ")?;
+            write_stmt(body, dx)?;
+            write!(dx, " else ")?;
+            write_stmt(alt, dx)
         }
         CStmt::VarDecl { name, ty, init_opt } => {
-            write_var_with_ty(name, ty, indent, out)?;
+            write_var_with_ty(name, ty, dx)?;
 
             if let Some(init) = init_opt {
-                write!(out, " = ")?;
-                write_expr(init, indent, out)?;
+                write!(dx, " = ")?;
+                write_expr(init, dx)?;
             }
 
-            write!(out, ";")
+            write!(dx, ";")
         }
         CStmt::FnDecl {
             name,
@@ -122,77 +169,80 @@ fn write_stmt(stmt: &CStmt, indent: usize, out: &mut Vec<u8>) -> io::Result<()> 
             result_ty,
             body,
         } => {
-            write_ty(result_ty, indent, out)?;
-            write!(out, " {}", name)?;
-            write_param_list(&params, indent, out)?;
-            write!(out, " ")?;
-            write_block(body, indent, out)
+            write_ty(result_ty, dx)?;
+            write!(dx, " {}", name)?;
+            write_param_list(&params, dx)?;
+            write!(dx, " ")?;
+            write_block(body, dx)
         }
         CStmt::ExternFnDecl {
             name,
             params,
             result_ty,
         } => {
-            write_ty(result_ty, indent, out)?;
-            write!(out, " {}", name)?;
-            write_param_list(&params, indent, out)?;
-            write!(out, ";")
+            write_ty(result_ty, dx)?;
+            write!(dx, " {}", name)?;
+            write_param_list(&params, dx)?;
+            write!(dx, ";")
         }
         CStmt::StructDecl { name, fields } => {
-            write!(out, "struct {} {{\n", name)?;
+            write!(dx, "struct {} {{\n", name)?;
 
             for (name, ty) in fields {
-                let indent = indent + 1;
-                write_indent(indent, out)?;
-                write_var_with_ty(&name, &ty, indent, out)?;
-                write!(out, ";")?;
-                write!(out, "\n")?;
+                dx.do_with_indent(|dx| {
+                    write_indent(dx)?;
+                    write_var_with_ty(&name, &ty, dx)?;
+                    write!(dx, ";")?;
+                    write!(dx, "\n")
+                })?;
             }
 
-            write_indent(indent, out)?;
-            write!(out, "}};")
+            write_indent(dx)?;
+            write!(dx, "}};")
         }
         CStmt::Label { .. } => unreachable!(),
     }
 }
 
-fn write_stmt_with_indent(stmt: &CStmt, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_stmt_with_indent(stmt: &CStmt, dx: &mut Dx<impl Write>) -> io::Result<()> {
     if let CStmt::Label { label } = stmt {
-        write!(out, "\n")?;
-        write_indent(indent.saturating_sub(1), out)?;
-        return write!(out, "{}:;", label);
+        return dx.do_with_unindent(|dx| {
+            write!(dx, "\n")?;
+            write_indent(dx)?;
+            write!(dx, "{}:;", label)
+        });
     }
 
-    write_indent(indent, out)?;
-    write_stmt(stmt, indent, out)
+    write_indent(dx)?;
+    write_stmt(stmt, dx)
 }
 
-fn write_block(block: &CBlock, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_block(block: &CBlock, dx: &mut Dx<impl Write>) -> io::Result<()> {
     let stmts = &block.stmts;
 
     if stmts.is_empty() {
-        write!(out, "{{}}")
+        write!(dx, "{{}}")
     } else {
-        write!(out, "{{\n")?;
+        write!(dx, "{{\n")?;
 
         for stmt in stmts {
-            write_stmt_with_indent(stmt, indent + 1, out)?;
-            write!(out, "\n")?;
+            dx.do_with_indent(|dx| write_stmt_with_indent(stmt, dx))?;
+            write!(dx, "\n")?;
         }
 
-        write_indent(indent, out)?;
-        write!(out, "}}")
+        write_indent(dx)?;
+        write!(dx, "}}")
     }
 }
 
-fn write_root(root: &CRoot, indent: usize, out: &mut Vec<u8>) -> io::Result<()> {
+fn write_root(root: &CRoot, dx: &mut Dx<impl Write>) -> io::Result<()> {
     for (i, decl) in root.decls.iter().enumerate() {
         if i != 0 {
-            write!(out, "\n")?;
+            write!(dx, "\n")?;
         }
 
-        write_stmt_with_indent(decl, indent, out)?;
-        write!(out, "\n")?;
+        write_stmt_with_indent(decl, dx)?;
+        write!(dx, "\n")?;
     }
     Ok(())
 }
@@ -201,8 +251,12 @@ pub(crate) fn clang_dump(k_root: KRoot, outlines: Rc<KOutlines>) -> String {
     let c_root = clang_gen::gen(k_root, outlines);
 
     let text = {
-        let mut out = vec![];
-        super::clang_dump::write_root(&c_root, 0, &mut out).unwrap();
+        let out = {
+            let mut dx = Dx::new(vec![]);
+            super::clang_dump::write_root(&c_root, &mut dx).unwrap();
+            dx.finish()
+        };
+
         String::from_utf8_lossy(&out).to_string()
     };
 
