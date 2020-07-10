15219eadf7c7aab09112725c6d358f91293264b9
vain0x <vainzerox@gmail.com>
2020-07-10 19:08:58 +0900

    feat: 終了コードを引数から受け取る

--- examples/c_compiler/c_compiler.c
+++ examples/c_compiler/c_compiler.c
@@ -1,14 +1,23 @@
 // Library wrappers and main function.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void cc_main();
 
-int jacco_std_io_write(unsigned char const *s) {
+int jacco_std_io_print(unsigned char const *s) {
     return fputs((char const *)s, stdout) != EOF;
 }
 
-int main() {
-    cc_main();
+int jacco_std_io_eprint(unsigned char const *s) {
+    return fputs((char const *)s, stderr) != EOF;
+}
+
+void jacco_std_process_exit(int code) {
+    exit(code);
+}
+
+int main(int argc, char **argv) {
+    cc_main((unsigned long long)argc, (unsigned char const *const *)argv);
     return 0;
 }
