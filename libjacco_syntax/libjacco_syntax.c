50a3f391def7b93cd2afd2614a964fa34b5c479e
vain0x <vainzerox@gmail.com>
2020-07-14 23:57:11 +0900

    refactor: 字句解析の結果を文字列にダンプすることで、テストケースを書きやすくする

--- libjacco_syntax/libjacco_syntax.c
+++ libjacco_syntax/libjacco_syntax.c
@@ -4,11 +4,16 @@
 #include <stdlib.h>
 
 typedef unsigned char u8;
+typedef unsigned long long usize;
 
 extern int libjacco_syntax_tests();
 
 void eprint_s(u8 const* value) {
     fprintf(stderr, "%s", value);
+}
+
+void eprint_t(u8 const* value, usize len) {
+    fprintf(stderr, "%.*s", (int)len, value);
 }
 
 void eprint_d(int value) {
